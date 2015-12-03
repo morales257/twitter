6eb4d3bfaebb5fea8e70fb22d6024b1efeb90645
Luis Morales-Diaz <luis@luisemorales.com>
2015-12-03 12:05:56 +0000

    Finish static pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,16 +4,20 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
+    #tests for the presence of a certain HTML tag (selector)
+    assert_select "title", "Home | Ruby on Rails Tutorial Twitter Clone"
   end
 
   test "should get help" do
     get :help
     assert_response :success
+    assert_select "title", "Help | Ruby on Rails Tutorial Twitter Clone"
   end
   
   test "should get about" do
     get :about
     assert_response :success
+    assert_select "title", "About | Ruby on Rails Tutorial Twitter Clone"
   end
 
 end
